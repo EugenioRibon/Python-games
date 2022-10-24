9cef06fe03405d7fa448e777ee63503bc6eb3e0a
Eugenio <112661310+EugenioRibon@users.noreply.github.com>
2022-10-24 14:11:01 +0200

    Add files via upload

---
+++ Pong game(2p).py
@@ -0,0 +1,126 @@
+
+import turtle
+import os 
+
+wn = turtle.Screen()
+wn.title("Pong by Eugenio Ribon") #Esto cambia el titulo 
+wn.bgcolor("black") #Para elegir el background color
+wn.setup(width=800, height=600) #configuracion de la pantalla
+wn.tracer(0)
+
+#Score
+score_a = 0
+score_b = 0
+
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350,0)
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+
+#Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350,0)
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+
+#ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+ball.dx = 2
+ball.dy = 2
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write(f"Player A: {score_a} Player B: {score_b}", align = "center", font = ("Courier", 24, "normal"))
+
+#Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+#Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+
+
+
+#Main game loop
+while True:
+    wn.update()
+
+    #Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        os.system("afplay Pong_sound.wav&")
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        os.system("afplay Pong_sound.wav&")
+    
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a} Player B: {score_b}", align = "center", font = ("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a} Player B: {score_b}", align = "center", font = ("Courier", 24, "normal"))
+
+    #Paddle and ball collision
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+        os.system("afplay Pong_sound.wav&")
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+        os.system("afplay Pong_sound.wav&")
+
